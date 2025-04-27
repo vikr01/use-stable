89563fdba5cac0e4d4460a4c940bd4038c16ed43
vikr01 <vik120@icloud.com>
2025-04-26 19:23:54 -0700

    support other useRef-like hooks in the builder

---
+++ src/builder/index.ts
@@ -0,0 +1,45 @@
+import { type useRef as useRefType } from "react";
+
+const SENTINEL: unique symbol = Symbol();
+
+export type UseStable = <T>(
+  creator: () => T,
+  dependencies: Array<unknown>,
+) => T;
+
+export default function builder(useRef: typeof useRefType): UseStable {
+  return function useStable<T>(
+    creator: () => T,
+    dependencies: Array<unknown>,
+  ): T {
+    const valueRef = useRef<typeof SENTINEL | T>(SENTINEL);
+    const depsRef = useRef<Array<unknown>>(dependencies);
+
+    if (
+      valueRef.current === SENTINEL ||
+      !isShallowEqual(depsRef.current, dependencies)
+    ) {
+      valueRef.current = creator();
+    }
+
+    depsRef.current = [...dependencies];
+
+    return valueRef.current;
+  };
+
+  function isShallowEqual(arr1: Array<unknown>, arr2: Array<unknown>): boolean {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+
+    const length = arr1.length;
+
+    for (let i = 0; i < length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,6 @@
 import { useRef } from "react";
+import builder, { type UseStable } from "./builder";
 
-const SENTINEL: unique symbol = Symbol();
+const useStable: UseStable = builder(useRef);
 
-export default function useStable<T>(
-  creator: () => T,
-  dependencies: Array<unknown>,
-): T {
-  const valueRef = useRef<typeof SENTINEL | T>(SENTINEL);
-  const depsRef = useRef<Array<unknown>>(dependencies);
-
-  if (
-    valueRef.current === SENTINEL ||
-    !isShallowEqual(depsRef.current, dependencies)
-  ) {
-    valueRef.current = creator();
-  }
-
-  depsRef.current = [...dependencies];
-
-  return valueRef.current;
-}
-
-function isShallowEqual(arr1: Array<unknown>, arr2: Array<unknown>): boolean {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  const length = arr1.length;
-
-  for (let i = 0; i < length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
+export default useStable;
